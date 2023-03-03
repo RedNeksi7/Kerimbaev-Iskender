9eb9791800ca88d20ae70bca29a595b65a369605
RedNeksi7 <126886432+RedNeksi7@users.noreply.github.com>
2023-03-03 23:44:28 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,27 +2,22 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        n(generateRandomAge(0,45),5);
-        n(generateRandomAge(0,45),24);
-        n(generateRandomAge(0,45),14);
-        n(generateRandomAge(0,45),30);
-        n(generateRandomAge(0,45),70);
+        temp_age(generateRandomAge(),18);
+        temp_age(generateRandomAge(),20);
+        temp_age(generateRandomAge(),46);
+        temp_age(generateRandomAge(),32);
+        temp_age(generateRandomAge(),10);
     }
-    public static void n(int age, int temp){
-        if (temp>-20&&temp<30&&age>20&&age<45){
-            System.out.println("Можно идти гулять.");
-        }else if(age<20&&temp<28&&temp>0){
-            System.out.println("Можно идти гулять.");
-        }else if(age>45&&temp>-10&&temp<25){
-            System.out.println("Можно идти гулять.");
-        } else {
+    public static void temp_age(int temp, int age){
+        if (age>20&&age<45&&temp>-20&&temp<30||age<20&&temp>0&&temp<28||age>45&&temp>-10&&temp<25){
+            System.out.println("Можно гулять.");
+        }else{
             System.out.println("Оставайтесь дома.");
         }
         return;
     }
-    public static int generateRandomAge(int min, int max) {
-        Random rand = new Random();
-        int age = rand.nextInt(max - min + 1) + min;
-        return age;
+    public static int generateRandomAge(){
+        int a = (int) (Math.random()*(30+1));
+        return a;
     }
 }
