c12d0c1cda051cfeb351a0e2ca1b64b705f4b5d0
RedNeksi7 <126886432+RedNeksi7@users.noreply.github.com>
2023-03-04 13:56:28 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,18 @@
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        temp_age(generateRandomAge(),18);
-        temp_age(generateRandomAge(),20);
-        temp_age(generateRandomAge(),46);
-        temp_age(generateRandomAge(),32);
-        temp_age(generateRandomAge(),10);
+        System.out.println(temp_age(20,generateRandomAge()));
+        System.out.println(temp_age(15,generateRandomAge()));
+        System.out.println(temp_age(10,generateRandomAge()));
+        System.out.println(temp_age(5,generateRandomAge()));
+        System.out.println(temp_age(25,generateRandomAge()));
     }
-    public static void temp_age(int temp, int age){
+    public static String temp_age(int temp, int age){
         if (age>20&&age<45&&temp>-20&&temp<30||age<20&&temp>0&&temp<28||age>45&&temp>-10&&temp<25){
-            System.out.println("Можно гулять.");
+            return "Можно идти гулять";
         }else{
-            System.out.println("Оставайтесь дома.");
+            return "Оставайтесь дома.";
         }
-        return;
+
     }
     public static int generateRandomAge(){
         int a = (int) (Math.random()*(30+1));
