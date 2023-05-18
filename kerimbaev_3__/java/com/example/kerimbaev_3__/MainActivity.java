e1d4555acb742ef86dfcbc3ab6c495fff55afd4a
RedNeksi <126886432+RedNeksi7@users.noreply.github.com>
2023-05-18 18:23:59 +0600

    Add files via upload

---
+++ kerimbaev_3__/java/com/example/kerimbaev_3__/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.kerimbaev_3__;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.kerimbaev_3__.adapter.ContactAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String>arrayListNames=new ArrayList<>();
+    ContactAdapter adapter;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        arrayListAdd();
+        adapter = new ContactAdapter(arrayListNames);
+        recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(adapter);
+    }
+    public void arrayListAdd(){
+        arrayListNames.add("Kerimbaev");
+        arrayListNames.add("Iskender");
+        arrayListNames.add("Talantbekovich");
+    }
+}
---
+++ kerimbaev_3__/java/com/example/kerimbaev_3__/adapter/ContactViewHolder.java
@@ -0,0 +1,26 @@
+package com.example.kerimbaev_3__.adapter;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kerimbaev_3__.R;
+
+public class ContactViewHolder extends RecyclerView.ViewHolder {
+    TextView textViewName;
+    ImageView icon;
+
+    public ContactViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void onBind(String name){
+        textViewName = itemView.findViewById(R.id.textViewName);
+        icon = itemView.findViewById(R.id.imageViewIcon);
+
+        textViewName.setText(name);
+    }
+}
