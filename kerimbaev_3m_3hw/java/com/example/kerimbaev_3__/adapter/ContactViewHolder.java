4de26f7fe606b1fc726e7fb679673e19bd836dde
RedNeksi <126886432+RedNeksi7@users.noreply.github.com>
2023-05-18 15:45:51 +0600

    Update ContactViewHolder.java

--- kerimbaev_3m_3hw/java/com/example/kerimbaev_3__/adapter/ContactViewHolder.java
+++ kerimbaev_3m_3hw/java/com/example/kerimbaev_3__/adapter/ContactViewHolder.java
@@ -3,10 +3,8 @@ package com.example.kerimbaev_3__.adapter;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.kerimbaev_3__.R;
 
 public class ContactViewHolder extends RecyclerView.ViewHolder {
